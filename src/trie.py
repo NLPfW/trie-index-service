92a254465cd61a43e3aa2e26b32b3342914190d3
Xiang Zhang <zxteloiv@outlook.com>
2016-07-05 21:33:46 +0800

    add trie index skeleton

---
+++ src/trie.py
@@ -0,0 +1,16 @@
+
+class TrieIndex(object):
+    def __init__(self):
+        pass
+
+    def add(self, key, encoding='utf-8'):
+        ukey = key.decode(encoding)
+        for i, u_char in enumerate(ukey):
+            pass
+        pass
+
+    def get(self, key):
+        pass
+
+    def matcher(self, string, begin):
+        pass
