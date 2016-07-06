32b93d482b2e78abb2a15c3cf0ab3b1826e33b5e
Xiang Zhang <zxteloiv@outlook.com>
2016-07-06 10:00:01 +0800

    change the return index meaning as the same with python style, add the beginning offset, too

--- src/trie.py
+++ src/trie.py
@@ -25,18 +25,28 @@ class TrieIndex(object):
 
         return "__val" in p and p["__val"] == 1
 
-    def matcher(self, string, begin=1):
-        string = string.decode('utf-8')
+    """
+    matcher: find the common prefix of a given string and the trie
+
+    @string the given string in bytes
+    @begin the start index, ignore the unicode chars before it
+
+    @return generator each time gives the index which it meets
+    """
+    def matcher(self, string, begin=0):
+        string = string.decode('utf-8')[begin:]
         p = self.root
         for i, u_char in enumerate(string):
             p = p[u_char]
             if "__val" in p and p["__val"] == 1:
-                yield i
+                yield i + 1
 
 if __name__ == "__main__":
     t = TrieIndex()
     t.add("abcd")
     t.add("abc")
+
+    print "'abcd' and 'abc' added into trie index"
 
     print "abc in trie:", t.get("abc")
     print "abcd in trie:", t.get("abcd")
@@ -44,4 +54,4 @@ if __name__ == "__main__":
     print "a not in trie:", t.get("a")
 
     for i in t.matcher("abcd"):
-        print "find prefix 0-%d:" % i, ("abcd")[0:(i + 1)]
+        print "find prefix 0-%d:" % i, ("abcd")[0:i]
