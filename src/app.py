80aedf7f93fa794273a8cd07f37db85211b2fecc
Xiang Zhang <zxteloiv@outlook.com>
2016-07-06 10:38:28 +0800

    add route and new app class

--- src/app.py
+++ src/app.py
@@ -1,14 +1,28 @@
 import tornado.ioloop
 import tornado.web
 
+import conf
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+class MatchHandler(tornado.web.RequestHandler):
+    def get(self):
+        pass
+
+class TISApp(tornado.web.Application):
+    def init_dict(self):
+        self.dicts = {}
+
 def make_app():
-    return tornado.web.Application([
+    app = TISApp([
         (r"/", MainHandler),
-    ])
+        (r"/match", MatchHandler),
+        ])
+
+    app.init_dict()
+    return app
 
 if __name__ == "__main__":
     app = make_app()
