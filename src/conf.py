5ab1bdb20c59b06995b531b2b803fbf9bbf5cfa0
Xiang Zhang <zxteloiv@outlook.com>
2016-07-11 15:55:41 +0800

    load the poi_class and nearby dict into config

--- src/conf.py
+++ src/conf.py
@@ -3,6 +3,8 @@ dict_path = "dicts"
 dict_conf = dict(
         poi = "poi.dict",
         location_func = "location.func.dict",
+        poi_class = "class.dict",
+        nearby = "nearby.dict",
         )
 
 listening_port = 9528
